85e5a1b325953a578256e6e1b15bd697bed7939f
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-02 03:56:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const namesArr = [];
+  for (let i = 0; i < names.length; i++) {
+    namesArr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return namesArr;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+countDown(10);
